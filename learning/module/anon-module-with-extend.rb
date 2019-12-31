80b46d08f5a927a4ebf4d0213bef9333b4012c37
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2019-12-31 11:46:34 -0300

    feat: Add example

---
+++ learning/module/anon-module-with-extend.rb
@@ -0,0 +1,11 @@
+my_mod = Module.new do
+  def custom_lower
+    puts "Current: #{self}."
+    self.downcase
+  end
+end
+
+name = "Luiz"
+name.extend(my_mod)
+
+puts "New value: #{name.custom_lower}"
