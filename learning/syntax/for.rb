ad83696ea66264501f59cbdd7756367d3f0812ea
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2019-12-31 20:56:21 -0300

    feat: Add examples

---
+++ learning/syntax/for.rb
@@ -0,0 +1,13 @@
+# if, unless, while, until, for, break, next, redo
+
+for value in (0..3).to_a do
+  if arg == 1
+    "Arg is one"
+  elsif arg == 2
+    "Arg is two"
+  else
+    "Arg is unknown"
+  end
+end
+
+# `unless` é o mesmo que `if !(cond)`
---
+++ learning/syntax/while.rb
@@ -0,0 +1,6 @@
+i = 1
+
+while i <= 5 do
+  puts i
+  i = i + 1
+end
